99e748c468e204aab14ded23782c36afa75ae7a2
martinsmihov@gmail.com <msm274@cca-lg04-12032.cs.bham.ac.uk>
2015-03-05 15:18:48 +0000

    Functions

--- host/sweep_host.cu
+++ host/sweep_host.cu
@@ -16,7 +16,63 @@ float sweep_radius;
 int number_sweep_steps;
 int number_ellipse_points;
 
-int main()
+// Read from file
+void readFromFile(char *filename, float *arr)
 {
+}
+
+// Write to file
+void writeToFile(char *filename, float* arr)
+{
+}
+
+// Matrix multiplication
+void matrix_mul(float *a, float *b, float *res)
+{
+}
+
+// Sweep the ellypse
+void sweep()
+{
+}
+
+// Generate the surface table
+void generateSurfaceTable()
+{
+}
+
+int main(int argc, char** argv)
+{
+	//CUDA properties
+	int devID;
+	cudaDeviceProp props;
+
+	// get number of SMs on this GPU
+	cutilSafeCall(cudaGetDevice(&devID));
+	cutilSafeCall(cudaGetDeviceProperties(&props, devID));
+
+        // create a timer
+	unsigned int timer = 0;
+        cutilCheckError(cutCreateTimer(&timer));
+	// start the timer
+        cutilCheckError(cutStartTimer(timer));
+
+	//
+	//
+	// DO STUFF HERE
+	//
+	//
+
+	// check if kernel execution generated and error
+	cutilCheckMsg("Kernel execution failed");
+
+	// wait for device to finish
+	cudaThreadSynchronize();
+
+	cutilCheckError(cutStopTimer(timer));
+
+	// exit and clean up device status
+	cudaThreadExit();
+
 	return 0;
 }
