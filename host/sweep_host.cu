053e847da8e552bb2721570e71fffcd74f377c95
Martin Mihov <martinsmihov@gmail.com>
2015-03-05 14:07:02 +0000

    Folder structure

---
+++ host/sweep_host.cu
@@ -0,0 +1,22 @@
+/// COMMENT
+
+#include "cutil_inline.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+float* h_ellipse_vertex;
+float* h_torus_vertex;
+float* h_torus_surface;
+
+float sweep_radius;
+int number_sweep_steps;
+int number_ellipse_points;
+
+int main()
+{
+	return 0;
+}
