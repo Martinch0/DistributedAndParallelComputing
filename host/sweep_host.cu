8f383327fa9b09d3b9843562344edf75614af294
Yu-Yang Lin <yxl265@cca-lg04-12031.cs.bham.ac.uk>
2015-03-05 15:53:14 +0000

    added comments to distribute work, added ellipse matrix txt file.

--- host/sweep_host.cu
+++ host/sweep_host.cu
@@ -11,34 +11,40 @@ using namespace std;
 float* h_ellipse_vertex;
 float* h_torus_vertex;
 float* h_torus_surface;
+float* sweep_origin;
 
-float sweep_radius;
 int number_sweep_steps;
 int number_ellipse_points;
 
 // Read from file
 void readFromFile(char *filename, float *arr)
 {
+  //Martin
 }
 
 // Write to file
 void writeToFile(char *filename, float* arr)
 {
+  //Martin
 }
 
 // Matrix multiplication
-void matrix_mul(float *a, float *b, float *res)
+float* matrix_mul(float *a, float *b, int ax, int ay, int bx, int by)
 {
+  float* result = new float[ax][by];
+  //Yu-Yang
 }
 
 // Sweep the ellypse
 void sweep()
 {
+  //Martin: do one ring after the other.
 }
 
 // Generate the surface table
 void generateSurfaceTable()
 {
+  //Yu-Yang: assumming rings are one after the other.
 }
 
 int main(int argc, char** argv)
@@ -50,12 +56,21 @@ int main(int argc, char** argv)
 	// get number of SMs on this GPU
 	cutilSafeCall(cudaGetDevice(&devID));
 	cutilSafeCall(cudaGetDeviceProperties(&props, devID));
-
-        // create a timer
+  
+  //
+  // INIT DATA HERE
+  //
+  
+  // print information
+  cout << "Number of ellipse vertices : " << number_ellipse_points << endl;
+  cout << "Number of rotational sweep steps : " << number_sweep_steps << endl;
+  cout << "Rotational sweep origin : " << "[" << sweep_origin[0] << ", " << sweep_origin[1] << ", " << sweep_origin[2] << "]" << endl;
+  
+  // create a timer
 	unsigned int timer = 0;
-        cutilCheckError(cutCreateTimer(&timer));
+  cutilCheckError(cutCreateTimer(&timer));
 	// start the timer
-        cutilCheckError(cutStartTimer(timer));
+  cutilCheckError(cutStartTimer(timer));
 
 	//
 	//
